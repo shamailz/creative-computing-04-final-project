94203489bf7e7f86a7f4349176d4736ae5d8f722
Shamail <zahis538@newschool.edu>
2018-12-04 11:43:40 -0500

    ok

--- js/page_2_site.js
+++ js/page_2_site.js
@@ -83,6 +83,12 @@ shapes.forEach(function(shape, i) {
   });
 });
 
+var link = document.querySelector('.special-link');
+link.style.top = 100 + 'px';
+link.style.left = 100 + 'px';
+link.style.zIndex = -1;
+
+
 
 
 
