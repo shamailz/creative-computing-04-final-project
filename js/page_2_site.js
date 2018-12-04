b1adbe2d23ca9201553b2cabf3c97c51a70770b2
Shamail <zahis538@newschool.edu>
2018-12-04 02:55:49 -0500

    Update js/page_2_site.js

--- js/page_2_site.js
+++ js/page_2_site.js
@@ -51,9 +51,22 @@ function hslColor(h, s, l) {
 /* START WRITING YOUR CODE BELOW */
 
 for (var i = 0; i < 300; i++) {
-  var shape= document.querySelector('.shape')
+  // create a div
+  var shape = document.createElement('div');
+  //add class "shape" to div
+  shape.classList.add('shape');
 }
+var shapes= document.querySelectorAll('.shape');
 
+shapes.forEach(function(shape, i) {
+  shape.addEventListener('mouseover', function(event) {
+      if(shape.classList.contains('shape-interaction')) {
+        shape.classlist.remove('shape-interaction');
+    } else {
+    shape.classList.add('shape-interaction');
+  }
+  });
+});
   
 
 
